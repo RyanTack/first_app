81926fd7a91c1e8379fd0c5a5299233d8ee8f0db
ysu60 <ysu60@ysu60-OptiPlex-745.(none)>
2011-03-21 10:35:17 +0000

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'eb75d490a56c81922222b33517b3b23c06885c13b0e82ade7c22794622fcc98885b47bd6bd1e8bdd4877299e4c00647188685d875a7a143118638750fbe94c71'
